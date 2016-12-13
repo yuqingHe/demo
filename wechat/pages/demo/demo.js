a6ce6ab643cec7a2f96a830ba0677448f2f9f13a
Heyuqing <yuqing.he@xiaobaoonline.com>
2016-12-13 10:16:27 +0800

    新增测试

--- wechat/pages/demo/demo.js
+++ wechat/pages/demo/demo.js
@@ -1,10 +1,33 @@
-//logs.js
 Page({
   data: {
-    mydata:"ddd",
-    data:{data:"123"}
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
   },
-  onLoad: function () {
-    console.log("demo loaded")
+  changeIndicatorDots: function(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function(e) {
+    this.setData({
+      duration: e.detail.value
+    })
   }
 })
--- wechat/pages/index/index.js
+++ wechat/pages/index/index.js
@@ -4,27 +4,62 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World90',
-    tapme:"你点我啊",
-    userInfo: {}
+    tapme: "你点我啊",
+    count: 1,
+    userInfo: {},
+    staffA: {
+      firstName: 'Hulk',
+      lastName: 'Hu'
+    },
+    staffB: {
+      firstName: 'Shang',
+      lastName: 'You'
+    },
+    staffC: {
+      firstName: 'Gideon',
+      lastName: 'Lin'
+    },
+    array: ["123", "345"]
+  },
+  add: function (e) {
+    this.setData({
+      count: this.data.count + 1
+    })
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  showtap :function(){
-     console.log("我被点钟了");
-     this.bindViewTap();
+  showtap: function () {
+    console.log("我被点钟了");
+    this.bindViewTap();
+  },
+
+  toswiper: function () {
+    wx.navigateTo({
+      url: '../demo/demo'
+    })
+  },
+  toIcon: function () {
+    wx.navigateTo({
+      url: '../icon/icon'
+    })
+  },
+  toScroll: function () {
+    wx.navigateTo({
+      url: '../scroll/scroll'
+    })
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+      //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
