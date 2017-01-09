1fec5cb4d66a0dce40d698be4b9aaa9f46e46209
Heyuqing <yuqing.he@xiaobaoonline.com>
2017-01-09 16:12:54 +0800

    ...

--- vue/demo/js/demo.js
+++ vue/demo/js/demo.js
@@ -1,7 +1,29 @@
+// 基础demo
 var demo1=new Vue({
     el:"#appFor",
     data:{
         message:"hello",
         todos:[{text:123},{text:4321}]
+    },
+    methods:{
+        showMessage:function (params) {
+            alert(this.message);
+            this.message="nichousha"
+        }
     }
 });
+
+//组件
+Vue.component('comp1', {
+   template: '<button v-on:click="counter2 += 1">{{ counter2 }}</button>',
+   props: ['counter'],
+   data:function () {
+    return {counter2:this.counter}
+  }
+});
+new Vue({
+  el: '#comp',
+  data:{
+      counter21:21
+  }
+})
